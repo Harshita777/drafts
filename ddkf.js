d72a9af34adecee81417ccb8709a41a835eeb43c
Harshita777 <68785551+Harshita777@users.noreply.github.com>
2024-08-16 17:22:21 +0530

    Update ddkf.js

--- ddkf.js
+++ ddkf.js
@@ -1,31 +1,67 @@
-{
-  "data": [
-    {
-      "pending-all": [
-        {
-          "count": "10",
-          "files": "6",
-          "amount": "130,600,00.00 AED"
-        }
-      ]
-    },
-    {
-      "telegraphics": [
-        {
-          "count": "8",
-          "files": "2",
-          "amount": "50,600,00.00 AED"
-        }
-      ]
-    },
-    {
-      "withinbank": [
-        {
-          "count": "4",
-          "files": "2",
-          "amount": "80,600,00.00 AED"
-        }
-      ]
-    }
-  ]
-}
+<Grid item xs={12}>
+  <Card className='shadow-none p-2 h-auto border rounded-1xl' elevation={3}>
+    <Box className='flex justify-between'>
+      <Text variant='h4' className='mt-4 ml-2 font-normal'> Pendings Activities </Text>
+      <Button onClick={handleRefresh} variant='tertiary'>Refresh</Button>
+    </Box>
+
+    <Box className='flex p-3 gap-5 '>
+      <Card className="shadow border mt-2 w-2/5 p-3 rounded-lg">
+        <Box className='flex justify-between'>
+          <Text variant='h5' className="font-normal">All</Text>
+          <IconButton className="text-gray-500"><ChevronRightSmall /></IconButton>
+        </Box>
+
+        <Text variant='label3' className="text-gray-500">
+          {pendingState?.data[0]?.['pending-all'][0]?.count} Individual Transactions
+        </Text>
+        <Div className='flex justify-between'>
+          <Text variant='label3' className="text-gray-500">
+            {pendingState?.data[0]?.['pending-all'][0]?.files} Files
+          </Text>
+          <Text variant='label3' className="text-md font-semibold text-gray-600">
+            {pendingState?.data[0]?.['pending-all'][0]?.amount}
+          </Text>
+        </Div>
+      </Card>
+
+      <Card className="shadow mt-2 border w-2/5 p-3 rounded-lg">
+        <Box className='flex justify-between'>
+          <Text variant="h5" className="font-normal">Telegraphic</Text>
+          <IconButton className="text-gray-500"><ChevronRightSmall /></IconButton>
+        </Box>
+
+        <Text variant='label3' className="text-gray-500">
+          {pendingState?.data[1]?.telegraphics[0]?.count} Individual Transactions
+        </Text>
+        <Div className='flex justify-between'>
+          <Text variant='label3' className="text-gray-500">
+            {pendingState?.data[1]?.telegraphics[0]?.files} Files
+          </Text>
+          <Text variant='label3' className="text-md font-semibold text-gray-600">
+            {pendingState?.data[1]?.telegraphics[0]?.amount}
+          </Text>
+        </Div>
+      </Card>
+
+      <Card className="shadow mt-2 border w-2/5 p-3 rounded-lg">
+        <Box className='flex justify-between'>
+          <Text variant="h5" className="font-normal">Within Bank</Text>
+          <IconButton className="text-gray-500"><ChevronRightSmall /></IconButton>
+        </Box>
+
+        <Text variant='label3' className="text-gray-500">
+          {pendingState?.data[2]?.withinbank[0]?.count} Individual Transactions
+        </Text>
+        <Div className='flex justify-between'>
+          <Text variant='label3' className="text-gray-500">
+            {pendingState?.data[2]?.withinbank[0]?.files} Files
+          </Text>
+          <Text variant='label3' className="text-md font-semibold text-gray-600">
+            {pendingState?.data[2]?.withinbank[0]?.amount}
+          </Text>
+        </Div>
+      </Card>
+    </Box>
+  </Card>
+</Grid>
