ecfa23a1a48038da04e3f13bd4504aa8484038c1
Harshita777 <68785551+Harshita777@users.noreply.github.com>
2024-08-18 19:23:10 +0530

    Update ddkf.js

--- ddkf.js
+++ ddkf.js
@@ -1,1 +1,14 @@
 
+npm install eslint --save-dev
+ 
+npx eslint --init
+ 
+for html report
+ 
+npm install eslint-html-reporter --save-dev
+ 
+inside package.json 
+ 
+scripts
+"lint":"eslint . -f node_modules/eslint-html-reporter/reporter.js -o eslint-report.html"
+ 
