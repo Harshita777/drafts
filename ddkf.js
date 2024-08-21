fcaba932a1b60e7ccedf2faf110d4b03558b2a16
Harshita777 <68785551+Harshita777@users.noreply.github.com>
2024-08-21 11:24:59 +0530

    Update ddkf.js

--- ddkf.js
+++ ddkf.js
@@ -1,19 +1,15 @@
 import React, { useState } from 'react';
 import { Tabs, Tab, Button, Menu, MenuItem, Box, Alert, Text, Div } from '@enbdleap/react-ui';
-import { ChevronUpSmall,ChevronDownSmall } from '@enbdleap/react-icons';
-import Payment from './Payment';
+import { ChevronUpSmall, ChevronDownSmall } from '@enbdleap/react-icons';
 import { useNavigate } from 'react-router-dom';
 import PendingActivities from './PendingActivities';
 
-
-
-
-const TabPendingActivities: React.FC = ({  }) => {
+const TabPendingActivities: React.FC = () => {
   const [value, setValue] = useState(0); 
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [subMenuOpen, setSubMenuOpen] = useState(false);
   const [hovering, setHovering] = useState(false); 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
@@ -22,28 +18,22 @@ const TabPendingActivities: React.FC = ({  }) => {
   const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
     setSubMenuOpen(false);
-    
   };
 
-  const handleClick =(value:string)=>{
-    if(value === "telegraphic"){
-      navigate("/dashboard/payments/telegraphic-bank-transfer")
-    } else if(value==="file-upload"){
-      navigate("/dashboard/payments/file-upload")
-    }else if(value==="within-bank-transfer"){
-      navigate("/dashboard/payments/within-bank-transfer")
+  const handleClick = (value: string) => {
+    if (value === "telegraphic") {
+      navigate("/dashboard/payments/telegraphic-bank-transfer");
+    } else if (value === "file-upload") {
+      navigate("/dashboard/payments/file-upload");
+    } else if (value === "within-bank-transfer") {
+      navigate("/dashboard/payments/within-bank-transfer");
     }
+  };
 
-    
-  }
   const handleClose = () => {
     setAnchorEl(null);
     setSubMenuOpen(false);
   };
-
-  // const handleSingleClick = () => {
-  //   setSubMenuOpen(!subMenuOpen);
-  // };
 
   const handleMouseEnter = () => {
     setHovering(true);
@@ -53,21 +43,14 @@ const TabPendingActivities: React.FC = ({  }) => {
     setHovering(false);
   };
 
-  
   const renderContent = () => {
     switch (value) {
       case 0:
-        return <Div>
-          <PendingActivities/>
-        </Div>;
+        return <PendingActivities transferType="all" />;
       case 1:
-        return <Div>
-        <PendingActivities/>
-      </Div>;
+        return <PendingActivities transferType="within-bank-transfer" />;
       case 2:
-        return <Div>
-        <PendingActivities/>
-      </Div>;
+        return <PendingActivities transferType="telegraphic-transfer" />;
       default:
         return <div></div>;
     }
@@ -75,74 +58,67 @@ const TabPendingActivities: React.FC = ({  }) => {
 
   return (
     <>
-    <Box className='flex justify-between w-full bg-blue-50'>
-      <Tabs
-        value={value}
-        onChange={handleTabChange}
-      >
-        <Tab label="All" />
-        <Tab label="Within Bank transfer" />
-        <Tab label="Telegraphic Transfer" />
-      </Tabs>
-      <Div>
-      <Box
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        position="relative"
-        display="inline-block"
-        className='flex '
-      >
-        {hovering && (
-          <Div className='h-10'>
-            <Alert  severity="info" className='h-10 flex align-center border rounded ' action={<Button color="secondary" size="small">Dismiss</Button>}
-            
+      <Box className='flex justify-between w-full bg-blue-50'>
+        <Tabs value={value} onChange={handleTabChange}>
+          <Tab label="All" />
+          <Tab label="Within Bank transfer" />
+          <Tab label="Telegraphic Transfer" />
+        </Tabs>
+        <Div>
+          <Box
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            position="relative"
+            display="inline-block"
+            className='flex '
+          >
+            {hovering && (
+              <Div className='h-10'>
+                <Alert
+                  severity="info"
+                  className='h-10 flex align-center border rounded '
+                  action={<Button color="secondary" size="small">Dismiss</Button>}
+                >
+                  <Text variant="h6">Your available current daily limit is 50,000.00 AED</Text>
+                </Alert>
+              </Div>
+            )}
+            <Button
+              size="medium"
+              onClick={handleButtonClick}
+              className="px-6 mt-1 mr-5"
             >
-              <Text variant="h6">Your available current daily limit is 50,000.00 AED</Text>
-            </Alert>
-          </Div>
-          
-        )}
-        <Button
-          size="medium"
-          onClick={handleButtonClick}
-          className="px-6 mt-1 mr-5"
-        >
-          Initiate Payment <ChevronDownSmall />
-        </Button>
-        
+              Initiate Payment <ChevronDownSmall />
+            </Button>
+          </Box>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+          >
+            <MenuItem>
+              Single <Div className='ml-20 mt-2'><ChevronUpSmall /></Div>
+            </MenuItem>
+
+            <Box sx={{ pl: 2 }}>
+              <MenuItem onClick={() => handleClick("telegraphic")}>Telegraphic Transfer</MenuItem>
+              <MenuItem onClick={() => handleClick("within-bank-transfer")}>Within Bank Transfer</MenuItem>
+            </Box>
+
+            <MenuItem onClick={() => handleClick("file-upload")}>File Upload</MenuItem>
+          </Menu>
+        </Div>
       </Box>
-      <Menu
-        anchorEl={anchorEl}
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-      >
-        <MenuItem>
-  Single <Div className='ml-20 mt-2'><ChevronUpSmall /></Div>
-</MenuItem>
-
-<Box sx={{ pl: 2 }}>
-  <MenuItem onClick={() => handleClick("telegraphic")}>Telegraphic Transfer</MenuItem>
-  <MenuItem onClick={() => handleClick("within-bank-transfer")}>Within Bank Transfer</MenuItem>
-</Box>
-
-<MenuItem onClick={() => handleClick("file-upload")}>File Upload</MenuItem>
-      </Menu>
-      </Div>
-      
-      
-      
-    </Box>
-    <Box mt={2}>{renderContent()}</Box>
+      <Box mt={2}>{renderContent()}</Box>
     </>
-    
   );
 };
 
@@ -152,36 +128,29 @@ export default TabPendingActivities;
 
 
 
-
-
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Box, Card, Grid, GridTable, Alert, Text, Tag, Button, IconButton, Div } from '@enbdleap/react-ui';
+import { Box, Card, Grid, GridTable, Text, Div } from '@enbdleap/react-ui';
 import { useNavigate } from 'react-router-dom';
-import TabsComponent from './TabsComponent';
-import { ChevronRightSmall } from '@enbdleap/react-icons';
 import { FETCH_TRANSACTION_SUMMARY_REQUEST } from '../../../redux/actions/DashboardActions';
 import { transactionSummaryColumns } from '../../../config/config';
 
-const PendingActivities = () => {
+interface PendingActivitiesProps {
+  transferType: string;
+}
+
+const PendingActivities: React.FC<PendingActivitiesProps> = ({ transferType }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const dashboardState = useSelector((state: any) => state.dashboardReducer);
-  const pendingState = useSelector((state: any) => state.pendingReducer);
-  const [value, setValue] = React.useState(0);
-  const transactionSummaryState =  useSelector((state: any) => state.transactionSummaryReducer);
+  const transactionSummaryState = useSelector((state: any) => state.transactionSummaryReducer);
 
-
- useEffect(() => {
-  dispatch({ type: FETCH_TRANSACTION_SUMMARY_REQUEST,payload:{
-    userId:"user001"
-  } });
-  
-}, [dispatch]);
+  useEffect(() => {
+    dispatch({ type: FETCH_TRANSACTION_SUMMARY_REQUEST, payload: { userId: "user001" } });
+  }, [dispatch]);
 
   const handleCellClick = (params: any) => {
     if (params.row && params.row.status && params.row.status.label) {
-      const status = params.row.status?.label;
+      const status = params.row.status.label;
       if (status === 'Pending') {
         navigate(`/dashboard/transaction?rfid=1234`);
       }
@@ -198,29 +167,33 @@ const PendingActivities = () => {
     'Conversion Failed': { label: 'Conversion Failed', type: 'error' },
   };
 
-  const rows = transactionSummaryState.data
-    ? transactionSummaryState.data.map((item: any, index: number) => ({
-        id: index + 1,
-        date: item.beneficiaryId.createdAt,
-        amount: item.debitAccountId.balance,
-        account: item.beneficiaryId.beneficiaryIBAN,
-        name: item.beneficiaryId.beneficiaryName,
-        customer: item.additionalDetails.customerReference,
-        fileType: item.transactionType.TransactionType,      
-        debit:item.debitAccountId.accountNumber,
-        accountName:item.debitAccountId.accountName,
-        local:item.paymentCurrency.currency,
-        payment:item.paymentDetails.paymentAmount,
-        currency:item.paymentCurrency.currency,
-        type:item.debitAccountId.accountType,
-        paymentDate:item.paymentDetails.paymentDate,
-        reference:item.beneficiaryId.beneficiaryReferenceId,
-        status: statusTags[item.transactionStatus.status] 
-      }))
+  const filteredRows = transactionSummaryState.data
+    ? transactionSummaryState.data
+        .filter((item: any) => 
+          transferType === 'all' || item.transactionType.TransactionType.toLowerCase().includes(transferType)
+        )
+        .map((item: any, index: number) => ({
+          id: index + 1,
+          date: item.beneficiaryId.createdAt,
+          amount: item.debitAccountId.balance,
+          account: item.beneficiaryId.beneficiaryIBAN,
+          name: item.beneficiaryId.beneficiaryName,
+          customer: item.additionalDetails.customerReference,
+          fileType: item.transactionType.TransactionType,
+          debit: item.debitAccountId.accountNumber,
+          accountName: item.debitAccountId.accountName,
+          local: item.paymentCurrency.currency,
+          payment: item.paymentDetails.paymentAmount,
+          currency: item.paymentCurrency.currency,
+          type: item.debitAccountId.accountType,
+          paymentDate: item.paymentDetails.paymentDate,
+          reference: item.beneficiaryId.beneficiaryReferenceId,
+          status: statusTags[item.transactionStatus.status],
+        }))
     : [];
 
   const GridTableProps = {
-    rows: rows,
+    rows: filteredRows,
     columns: [
       ...transactionSummaryColumns,
       {
@@ -233,12 +206,6 @@ const PendingActivities = () => {
           </div>
         ),
       },
-      {
-        field: 'rejection',
-        flex: 1,
-        headerName: 'Rejection Reason',
-      }
-     
     ],
     hidePagination: false,
     checkboxSelection: false,
@@ -254,91 +221,12 @@ const PendingActivities = () => {
     hideFooterRowCount: false,
   };
 
-  
-
-  
-
   return (
     <>
       <Grid container className='w-full h-auto shadow-bottom' margin={0}>
-        <Card className='bg-blue-50 w-full flex justify-between'>
-          
-        </Card>
-       
+        <Card className='bg-blue-50 w-full flex justify-between'></Card>
       </Grid>
       <Grid container spacing={2} className='p-9'>
-        <Grid item xs={12}>
-          <Card className='flex  shadow-none  p-2 h-auto border rounded-1xl' >
-            <Box className='flex flex-1 p-3 gap-5 '>
-              
-                
-                  <Card  className='shadow w-full border mt-2 p-3 rounded-lg'>
-                    <Box className='flex justify-between'>
-                      <Text variant='h5' className='font-normal'>
-                        16
-                      </Text>
-                      
-                    </Box>
-                    <Text variant='label3' className='text-gray-500'>
-                      Total
-                    </Text>
-                    <Div className='flex justify-between'>
-                     
-                      <Text variant='label3' className='text-md font-semibold text-gray-600'>
-                        10,500,000 AED
-                      </Text>
-                    </Div>
-                  </Card>
-               
-            </Box>
-            <Box className='flex flex-1 p-3 gap-5'>
-              
-                
-                  <Card  className='shadow w-full border mt-2 w-2/5 p-3 rounded-lg'>
-                    <Box className='flex justify-between'>
-                      <Text variant='h5' className='font-normal'>
-                        16
-                      </Text>
-                      
-                    </Box>
-                    <Text variant='label3' className='text-gray-500'>
-                      Total
-                    </Text>
-                    <Div className='flex justify-between'>
-                     
-                      <Text variant='label3' className='text-md font-semibold text-gray-600'>
-                        10,500,000 AED
-                      </Text>
-                    </Div>
-                  </Card>
-               
-            </Box>
-            <Box className='flex flex-1 p-3 gap-5'>
-              
-                
-                  <Card  className='shadow w-full border mt-2 w-2/5 p-3 rounded-lg'>
-                    <Box className='flex justify-between'>
-                      <Text variant='h5' className='font-normal'>
-                        16
-                      </Text>
-                      
-                    </Box>
-                    <Text variant='label3' className='text-gray-500'>
-                      Total
-                    </Text>
-                    <Div className='flex justify-between'>
-                     
-                      <Text variant='label3' className='text-md font-semibold text-gray-600'>
-                        10,500,000 AED
-                      </Text>
-                    </Div>
-                  </Card>
-               
-            </Box>
-          </Card>
-          
-        </Grid>
-
         <Grid item xs={12}>
           <Card className='shadow-none mt-5 p-2 h-auto border rounded-1xl' elevation={3}>
             <Box className='flex justify-between'>
@@ -347,11 +235,11 @@ const PendingActivities = () => {
               </Text>
             </Box>
             <Text variant='label1' className='text-gray-400'>
-              Showing 1 - 10 out of 16
+              Showing 1 - 10 out of {filteredRows.length}
             </Text>
 
-            {rows.length > 0 && (
-              <GridTable className='mt-4 border-none cursor-pointer' {...GridTableProps} />
+            {filteredRows.length > 0 && (
+              <GridTable className='mt-4 text-gray-600' {...GridTableProps} />
             )}
           </Card>
         </Grid>
