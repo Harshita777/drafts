39663b31783ef1fe21fd253a2a80a6947e93d64e
Harshita777 <68785551+Harshita777@users.noreply.github.com>
2024-08-21 13:27:03 +0530

    Update ddkf.js

--- ddkf.js
+++ ddkf.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Box, Card, Grid, GridTable, Text, Div, Tag } from '@enbdleap/react-ui';
+import { Box, Card, Grid, GridTable, Text, Div, Tag, Tabs, Tab } from '@enbdleap/react-ui';
 import { useNavigate } from 'react-router-dom';
 import { FETCH_TRANSACTION_SUMMARY_REQUEST } from '../../../redux/actions/DashboardActions';
-import { statusTags, transactionPendingColumns, transactionSummaryColumns } from '../../../config/config';
+import { statusTags, transactionPendingColumns } from '../../../config/config';
 
 interface PendingActivitiesProps {
   transferType: string;
@@ -13,6 +13,8 @@ const PendingActivities: React.FC<PendingActivitiesProps> = ({ transferType }) =
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const transactionSummaryState = useSelector((state: any) => state.transactionSummaryReducer);
+
+  const [selectedTransferType, setSelectedTransferType] = useState<string>(transferType);
 
   useEffect(() => {
     dispatch({ type: FETCH_TRANSACTION_SUMMARY_REQUEST, payload: { userId: "user001" } });
@@ -27,12 +29,15 @@ const PendingActivities: React.FC<PendingActivitiesProps> = ({ transferType }) =
     }
   };
 
- 
+  const handleTabChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+    setSelectedTransferType(newValue);
+  };
 
   const filteredRows = transactionSummaryState.data
     ? transactionSummaryState.data
         .filter((item: any) => 
-          transferType === 'all' || item.transactionType.TransactionType.includes(transferType)
+          item.transactionStatus.status === 'pending' && 
+          (selectedTransferType === 'all' || item.transactionType.TransactionType.includes(selectedTransferType))
         )
         .map((item: any, index: number) => ({
           id: index + 1,
@@ -78,79 +83,19 @@ const PendingActivities: React.FC<PendingActivitiesProps> = ({ transferType }) =
       <Grid container className='w-full h-auto shadow-bottom' margin={0}>
         <Card className='bg-blue-50 w-full flex justify-between'></Card>
       </Grid>
+
+      <Tabs value={selectedTransferType} onChange={handleTabChange} className="my-4">
+        <Tab label="Telegraphic Transfer" value="Telegraphic Transfer" />
+        <Tab label="Within Bank Transfer" value="Within Bank Transfer" />
+      </Tabs>
       
       <Grid container spacing={2} className='p-9'>
-      <Grid item xs={12}>
-          <Card className='flex shadow-none  p-2 h-auto border rounded-1xl' >
-            <Box className='flex  flex-1 p-3 gap-5 '>
-
-
-              <Card className='shadow w-full border mt-2 p-3 rounded-lg'>
-                <Box className='flex justify-between'>
-                  <Text variant='h5' className='font-normal'>
-                    16
-                  </Text>
-
-                </Box>
-                <Text variant='label3' className='text-gray-500'>
-                  Total
-                </Text>
-                <Div className='flex justify-between'>
-
-                  <Text variant='label3' className='text-md font-semibold text-gray-600'>
-                    10,500,000 AED
-                  </Text>
-                </Div>
-              </Card>
-
-            </Box>
-            <Box className='flex  flex-1 p-3 gap-5'>
-
-
-              <Card className='shadow w-full border mt-2 w-2/5 p-3 rounded-lg'>
-                <Box className='flex justify-between'>
-                  <Text variant='h5' className='font-normal'>
-                    16
-                  </Text>
-
-                </Box>
-                <Text variant='label3' className='text-gray-500'>
-                  Total
-                </Text>
-                <Div className='flex justify-between'>
-
-                  <Text variant='label3' className='text-md font-semibold text-gray-600'>
-                    10,500,000 AED
-                  </Text>
-                </Div>
-              </Card>
-
-            </Box>
-            <Box className='flex flex-1 p-3 gap-5'>
-
-
-              <Card className='shadow w-full border mt-2 w-2/5 p-3 rounded-lg'>
-                <Box className='flex justify-between'>
-                  <Text variant='h5' className='font-normal'>
-                    16
-                  </Text>
-
-                </Box>
-                <Text variant='label3' className='text-gray-500'>
-                  Total
-                </Text>
-                <Div className='flex justify-between'>
-
-                  <Text variant='label3' className='text-md font-semibold text-gray-600'>
-                    10,500,000 AED
-                  </Text>
-                </Div>
-              </Card>
-
-            </Box>
+        <Grid item xs={12}>
+          <Card className='flex shadow-none p-2 h-auto border rounded-1xl'>
+            {/* Add any content you want here */}
           </Card>
-
         </Grid>
+
         <Grid item xs={12}>
           <Card className='shadow-none mt-5 p-2 h-auto border rounded-1xl' elevation={3}>
             <Box className='flex justify-between'>
