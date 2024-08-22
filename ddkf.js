8d338807141ed39926a65bed116ac6853fc1fa14
Harshita777 <68785551+Harshita777@users.noreply.github.com>
2024-08-22 19:14:24 +0530

    Update ddkf.js

--- ddkf.js
+++ ddkf.js
@@ -1,44 +1,14 @@
-{
-  "data": [
-    {
-      "transactionType": {
-        "name": "Within Bank Transfer"
-      },
-      "debitedAmount": 1000,
-      "paymentAmount": 897,
-      "paymentCurrency": "AED",
-      "transactionStatus": {
-        "status": "Approved"
-      },
-      "transactionId": "-O4igiA0OgZn_FLcika0",
-      "referenceId": "DEW012024UTL",
-      "initiateDate": "2024-08-20T00:00:00.000Z"
-    },
-    {
-      "transactionType": {
-        "name": "Within Bank Transfer"
-      },
-      "debitedAmount": 2000,
-      "paymentAmount": 796,
-      "paymentCurrency": "AED",
-      "transactionStatus": {
-        "status": "Pending Authorization"
-      },
-      "transactionId": "-O4jxKyChmi7pDzSwbG_",
-      "referenceId": "DEW012024UTL",
-      "initiateDate": "somedate"
-    },
-    {
-      "debitedAmount": "1",
-      "paymentAmount": "2",
-      "transactionType": {
-        "name": "Within Bank Transfer"
-      },
-      "paymentCurrency": "USD",
-      "transactionStatus": {
-        "status": "Pending Authorization"
-      },
-      "transactionId": "-O4oIf48Y5EadrSICCCl",
-      "referenceId": "DEW1724233199944UTL",
-      "initiateDate": "2024-08-22T09:39:13.000Z"
-    },]
+interface TransactionDTO {
+  transactionType: {
+    name: string;
+  };
+  debitedAmount: number;
+  paymentAmount: number;
+  paymentCurrency: string;
+  transactionStatus: {
+    status: string;
+  };
+  transactionId: string;
+  referenceId: string;
+  initiateDate: string; // Consider using Date if you plan to handle this as a Date object.
+}
