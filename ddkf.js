03ff974090f4ad209026f631cbb8061ea4bf5255
Harshita777 <68785551+Harshita777@users.noreply.github.com>
2024-08-29 23:03:09 +0530

    Update ddkf.js

--- ddkf.js
+++ ddkf.js
@@ -1,3 +1,14 @@
+const formatCurrency = (amount: number, currency: string) => {
+    return new Intl.NumberFormat('en-IN', {
+        style: 'currency',
+        currency: currency,
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    }).format(amount);
+};
+
+
+
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { FETCH_DASHBOARD_REQUEST } from '../../redux/actions/DashboardActions';
@@ -8,39 +19,31 @@ import { recentTransactionsColumns, statusTags } from '../../config/config';
 import { useNavigate } from 'react-router-dom';
 
 const Dashboard: React.FC = () => {
-    // Initialize hooks for Redux dispatch, navigation, and retrieving the state from the Redux store.
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const dashboardState = useSelector((state: any) => state.dashboardReducer);
 
-    // State to manage filtered data based on category selection.
     const [filteredData, setFilteredData] = useState<any[]>([]);
     const [selectedCategory, setSelectedCategory] = useState<string>('pending-all');
 
-    // Fetch dashboard data when the component is mounted or when the dispatch is updated.
     useEffect(() => {
         dispatch({ type: FETCH_DASHBOARD_REQUEST });
     }, [dispatch]);
 
-    // Filter the table data whenever dashboard data or the selected category changes.
     useEffect(() => {
         if (dashboardState.data) {
             filterTableData(selectedCategory);
         }
     }, [dashboardState.data, selectedCategory]);
 
-    // Function to refresh the dashboard data.
     const handleRefresh = () => {
         dispatch({ type: FETCH_DASHBOARD_REQUEST });
     };
 
-
-    // Function to filter data based on the selected category.
     const filterTableData = (category: string) => {
         const data = dashboardState.data;
         let filtered = [];
 
-        // Filter based on the selected category.
         if (category === 'pending-all') {
             filtered = data;
         } else if (category === 'telegraphics') {
@@ -52,12 +55,19 @@ const Dashboard: React.FC = () => {
         setFilteredData(filtered);
     };
 
-    // Handle click events for category cards.
     const handleCardClick = (category: string) => {
         setSelectedCategory(category);
     };
 
-    // Define table columns, including a custom rendering for the status column.
+    const formatCurrency = (amount: number, currency: string = 'AED') => {
+        return new Intl.NumberFormat('en-IN', {
+            style: 'currency',
+            currency: currency,
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+        }).format(amount);
+    };
+
     const columns = [
         ...recentTransactionsColumns,
         {
@@ -74,21 +84,19 @@ const Dashboard: React.FC = () => {
             field: 'reference',
             flex: 1,
             headerName: 'Reference ID',
-          },
+        },
     ];
 
-    // Prepare table rows based on the filtered data.
     const rows = filteredData?.map((item: any, index: number) => ({
         id: index + 1,
         type: item.transactionType?.name,
         date: item.initiateDate,
-        amount: `${item.paymentAmount} ${item.paymentCurrency}`,
+        amount: formatCurrency(item.paymentAmount, item.paymentCurrency || 'AED'),
         status: statusTags[item.transactionStatus.status],
-        transactionId: item.transactionId,        
-		reference: item.referenceId,
+        transactionId: item.transactionId,
+        reference: item.referenceId,
     })) || [];
 
-    // Configuration for the DataGrid component.
     const GridTableProps = {
         rows: rows.reverse(),
         columns: columns,
@@ -101,10 +109,8 @@ const Dashboard: React.FC = () => {
         hideFooterRowCount: true,
     };
 
-    // Check if there is data available for the dashboard.
     const hasDashboardData = dashboardState.data && dashboardState.data.length > 0;
 
-    // Function to get details for each category card based on the category type.
     const getCardDetails = (category: string) => {
         switch (category) {
             case 'pending-all':
@@ -128,28 +134,25 @@ const Dashboard: React.FC = () => {
         }
     };
 
-    // Function to handle row clicks in the data grid
-	const handleCellClick = (params: any) => {
-		if (params.row && params.row.status && params.row.status.label) {
-			const status = params.row.status?.label;
-			const type = params.row.fileType;
-			const typeUrl = type.toString().toLowerCase().split(' ').join('-');
-			const rfid = params.row.referenceId
+    const handleCellClick = (params: any) => {
+        if (params.row && params.row.status && params.row.status.label) {
+            const status = params.row.status?.label;
+            const type = params.row.fileType;
+            const typeUrl = type.toString().toLowerCase().split(' ').join('-');
+            const rfid = params.row.referenceId;
 
-			// Conditional routing based on the transaction status and type
-			if (((status === 'Ready for Verification')) && type === "File Upload") {
-				navigate(`/dashboard/payments/file-verify`, { state: rfid });
-			} else if (status === 'Pending Authorization' && (type === "Telegraphic Transfer" || type === "Within Bank Transfer")) {
-				navigate(`/dashboard/payments/${typeUrl}?rfId=${rfid}`);
-			} else if (status === 'Pending Authorization' && type === "File Upload") {
-				navigate(`/dashboard/payments/within-bank-transfer?rfId=${rfid}&type=${typeUrl}`);
-			}
-		}
-	};
+            if (((status === 'Ready for Verification')) && type === "File Upload") {
+                navigate(`/dashboard/payments/file-verify`, { state: rfid });
+            } else if (status === 'Pending Authorization' && (type === "Telegraphic Transfer" || type === "Within Bank Transfer")) {
+                navigate(`/dashboard/payments/${typeUrl}?rfId=${rfid}`);
+            } else if (status === 'Pending Authorization' && type === "File Upload") {
+                navigate(`/dashboard/payments/within-bank-transfer?rfId=${rfid}&type=${typeUrl}`);
+            }
+        }
+    };
 
     return (
         <Grid container spacing={2}>
-            {/* Header and category cards */}
             <Grid item xs={12}>
                 <Card className='h-auto' elevation={0} sx={{ p: 2 }}>
                     <Flex direction="row" justifyContent="space-between">
@@ -157,7 +160,6 @@ const Dashboard: React.FC = () => {
                         <Button type="button" onClick={handleRefresh} variant='text'>Refresh</Button>
                     </Flex>
 
-                    {/* Category cards */}
                     <Box className='flex gap-5'>
                         {['pending-all', 'telegraphics', 'withinbank'].map((category, index) => {
                             const details = getCardDetails(category);
@@ -179,7 +181,7 @@ const Dashboard: React.FC = () => {
                                             <Text variant='body2' className="text-gray-500 font-medium">
                                                 <Text variant='label2' className='text-gray-800  font-semibold'>
                                                     {details.count}
-                                                    </Text> Single Transactions
+                                                </Text> Single Transactions
                                             </Text>
                                             <Text variant='body2' className="text-gray-500 font-medium">
                                                 <Text variant='label2' className='text-gray-800 font-semibold'>
@@ -196,7 +198,6 @@ const Dashboard: React.FC = () => {
                 </Card>
             </Grid>
 
-            {/* Data grid for recent transactions */}
             {hasDashboardData && (
                 <Grid item xs={12}>
                     <Card className='shadow-none h-auto border rounded-1xl' elevation={0} sx={{ p: 2 }}>
